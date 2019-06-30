aa82c1636dd777e33f80a214881e2822f4ee1c97
Andrei Lysiuk <metilpropenilen@gmail.com>
2019-06-30 07:58:52 +0300

    update css to scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/style.css';
+import './style/style.scss';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
